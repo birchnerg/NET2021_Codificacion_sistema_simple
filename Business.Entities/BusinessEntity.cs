17188600ff7492ed35de83603b73e55ce2436acd
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-26 19:02:54 -0300

    Update BusinessEntity.cs

--- Business.Entities/BusinessEntity.cs
+++ Business.Entities/BusinessEntity.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Business.Entities
 {
     public class BusinessEntity
     {
+        public enum States { 
+            Deleted,
+            New,
+            Modified,
+            Unmodified
+        }
+        public BusinessEntity()
+        {
+            this.State = States.New;
+        }
+        private States _State;
+        public States State
+        {
+            get { return _State; }
+            set { _State = value; }
+        }
+
+        private int _ID;
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
     }
 }
