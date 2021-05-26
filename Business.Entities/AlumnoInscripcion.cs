7d965f6ce2f29716e2633b845c72c11c4ec5da60
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-05-26 20:23:22 -0300

    Add classes

---
+++ Business.Entities/AlumnoInscripcion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class AlumnoInscripcion : BusinessEntity
+    {
+        private string _Condicion;
+        public string Condicion
+        {
+            get { return _Condicion; }
+            set { _Condicion = value; }
+        }
+        private int _IDAlumno;
+        public int IDAlumno
+        {
+            get { return _IDAlumno; }
+            set { _IDAlumno = value; }
+        }
+        private int _IDCurso;
+        public int IDCurso
+        {
+            get { return _IDCurso; }
+            set { _IDCurso = value; }
+        }
+        private int _Nota;
+        public int Nota
+        {
+            get { return _Nota; }
+            set { _Nota = value; }
+        }
+    }
+}
