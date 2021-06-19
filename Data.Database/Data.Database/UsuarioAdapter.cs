a432b423a1fdb895a7f63cc3f7f2c88f73b2ef14
pablo-maker <61093146+pablo-maker@users.noreply.github.com>
2021-06-19 14:11:15 -0300

    Finish Lab.05

--- Data.Database/Data.Database/UsuarioAdapter.cs
+++ Data.Database/Data.Database/UsuarioAdapter.cs
@@ -65,70 +65,186 @@ namespace Data.Database
         {
             //return new List<Usuario>(Usuarios);
             List<Usuario> usuarios = new List<Usuario>();
-
-            this.OpenConnection();
-
-            //Objeto SqlCommand para la sentencia SQL que se va a ejecutar
-            SqlCommand cmdUsuarios = new SqlCommand("SELECT * FROM usuarios", sqlConn);
-
-            //DataReader para recuperar los datos de la DB
-            SqlDataReader drUsuario = cmdUsuarios.ExecuteReader();
-
-            //Read() lee una fila de las devueltas por el commandSql, devuelve true mientras pueda leer datos y avanza 
-            //a la siguiente fila
-            while (drUsuario.Read())
+            try
             {
-                //Creamos un obj Usuario de la capa de entidades para copiar los datos del DataReader
-                Usuario usr = new Usuario();
+                this.OpenConnection();
 
-                usr.ID = (int)drUsuario["id_usuario"];
-                usr.NombreUsuario = (string)drUsuario["nombre_usuario"];
-                usr.Clave = (string)drUsuario["clave"];
-                usr.Habilitado = (bool)drUsuario["habilitado"];
-                usr.Nombre = (string)drUsuario["nombre"];
-                usr.Apellido = (string)drUsuario["apellido"];
-                usr.Email = (string)drUsuario["email"];
+                //Objeto SqlCommand para la sentencia SQL que se va a ejecutar
+                SqlCommand cmdUsuarios = new SqlCommand("SELECT * FROM usuarios", sqlConn);
 
-                usuarios.Add(usr);
+                //DataReader para recuperar los datos de la DB
+                SqlDataReader drUsuario = cmdUsuarios.ExecuteReader();
+
+                //Read() lee una fila de las devueltas por el commandSql, devuelve true mientras pueda leer datos y avanza 
+                //a la siguiente fila
+                while (drUsuario.Read())
+                {
+                    //Creamos un obj Usuario de la capa de entidades para copiar los datos del DataReader
+                    Usuario usr = new Usuario();
+
+                    usr.ID = (int)drUsuario["id_usuario"];
+                    usr.NombreUsuario = (string)drUsuario["nombre_usuario"];
+                    usr.Clave = (string)drUsuario["clave"];
+                    usr.Habilitado = (bool)drUsuario["habilitado"];
+                    usr.Nombre = (string)drUsuario["nombre"];
+                    usr.Apellido = (string)drUsuario["apellido"];
+                    usr.Email = (string)drUsuario["email"];
+
+                    usuarios.Add(usr);
+                }
+                drUsuario.Close();
             }
-            drUsuario.Close();
-            this.CloseConnection();
-
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar lista de usuarios", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
             return usuarios;
         }
 
         public Business.Entities.Usuario GetOne(int ID)
         {
-            return Usuarios.Find(delegate(Usuario u) { return u.ID == ID; });
+            //return Usuarios.Find(delegate(Usuario u) { return u.ID == ID; });
+            Usuario usr = new Usuario();
+            try
+            {
+                this.OpenConnection();
+
+                //Objeto SqlCommand para la sentencia SQL que se va a ejecutar
+                SqlCommand cmdUsuarios = new SqlCommand("SELECT * FROM usuarios WHERE id_usuario=@id", sqlConn);
+                cmdUsuarios.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+
+                //DataReader para recuperar los datos de la DB
+                SqlDataReader drUsuario = cmdUsuarios.ExecuteReader();
+
+                //Read() lee una fila de las devueltas por el commandSql, devuelve true mientras pueda leer datos
+                if (drUsuario.Read())
+                {
+                    usr.ID = (int)drUsuario["id_usuario"];
+                    usr.NombreUsuario = (string)drUsuario["nombre_usuario"];
+                    usr.Clave = (string)drUsuario["clave"];
+                    usr.Habilitado = (bool)drUsuario["habilitado"];
+                    usr.Nombre = (string)drUsuario["nombre"];
+                    usr.Apellido = (string)drUsuario["apellido"];
+                    usr.Email = (string)drUsuario["email"];
+
+                }
+                drUsuario.Close();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al recuperar datos del usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+            return usr;
         }
 
         public void Delete(int ID)
         {
-            Usuarios.Remove(this.GetOne(ID));
+            //Usuarios.Remove(this.GetOne(ID));
+            try
+            {
+                this.OpenConnection();
+
+                //Objeto SqlCommand para la sentencia SQL que se va a ejecutar
+                SqlCommand cmdDelete = new SqlCommand("DELETE usuarios WHERE id_usuario=@id", sqlConn);
+                cmdDelete.Parameters.Add("@id", SqlDbType.Int).Value = ID;
+                //Ejecutamos la sentencia
+                cmdDelete.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al eliminar usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Update(Usuario usuario)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("UPDATE usuarios SET nombre_usuario = @nombre_usuario," +
+                    "clave = @clave, habilitado = @habilitado, nombre = @nombre, apellido = @apellido," +
+                    "email = @email WHERE id_usuario  = @id",sqlConn);
+
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = usuario.ID;
+                cmdSave.Parameters.Add("@nombre_usuario", SqlDbType.VarChar, 50).Value = usuario.NombreUsuario;
+                cmdSave.Parameters.Add("@clave", SqlDbType.VarChar, 50).Value = usuario.Clave;
+                cmdSave.Parameters.Add("@habilitado", SqlDbType.Bit).Value = usuario.Habilitado;
+                cmdSave.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = usuario.Nombre;
+                cmdSave.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = usuario.Apellido;
+                cmdSave.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = usuario.Email;
+                cmdSave.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al modificar datos del usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
+        }
+
+        protected void Insert(Usuario usuario)
+        {
+            try
+            {
+                this.OpenConnection();
+                SqlCommand cmdSave = new SqlCommand("INSERT INTO usuarios (nombre_usuario,clave,habilitado," +
+                    "nombre,apellido,email) VALUES(@nombre_usuario,@clave,@habilitado," +
+                    "@nombre,@apellido,@email)" +
+                    "SELECT @@identity", //Recupera el ID que asigno el SQL automaticamente
+                    sqlConn);
+
+                cmdSave.Parameters.Add("@id", SqlDbType.Int).Value = usuario.ID;
+                cmdSave.Parameters.Add("@nombre_usuario", SqlDbType.VarChar, 50).Value = usuario.NombreUsuario;
+                cmdSave.Parameters.Add("@clave", SqlDbType.VarChar, 50).Value = usuario.Clave;
+                cmdSave.Parameters.Add("@habilitado", SqlDbType.Bit).Value = usuario.Habilitado;
+                cmdSave.Parameters.Add("@nombre", SqlDbType.VarChar, 50).Value = usuario.Nombre;
+                cmdSave.Parameters.Add("@apellido", SqlDbType.VarChar, 50).Value = usuario.Apellido;
+                cmdSave.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = usuario.Email;
+                usuario.ID = Decimal.ToInt32((decimal)cmdSave.ExecuteScalar());
+                //asi se obtiene el id que se asigno al DB automaticamente
+            }
+            catch (Exception Ex)
+            {
+                Exception ExcepcionManejada = new Exception("Error al crear usuario", Ex);
+                throw ExcepcionManejada;
+            }
+            finally
+            {
+                this.CloseConnection();
+            }
         }
 
         public void Save(Usuario usuario)
         {
-            if (usuario.State == BusinessEntity.States.New)
-            {
-                int NextID = 0;
-                foreach (Usuario usr in Usuarios)
-                {
-                    if (usr.ID > NextID)
-                    {
-                        NextID = usr.ID;
-                    }
-                }
-                usuario.ID = NextID + 1;
-                Usuarios.Add(usuario);
-            }
-            else if (usuario.State == BusinessEntity.States.Deleted)
+            if (usuario.State == BusinessEntity.States.Deleted)
             {
                 this.Delete(usuario.ID);
             }
+            else if (usuario.State == BusinessEntity.States.New)
+            {
+                this.Insert(usuario);
+            }
             else if (usuario.State == BusinessEntity.States.Modified)
             {
-                Usuarios[Usuarios.FindIndex(delegate(Usuario u) { return u.ID == usuario.ID; })]=usuario;
+                this.Update(usuario);
             }
             usuario.State = BusinessEntity.States.Unmodified;            
         }
