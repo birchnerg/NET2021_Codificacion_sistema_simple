9ba166339ff67e44a784d21b6d258186481f31eb
Milton Sotto Diaz <sottodiaz.milton@gmail.com>
2021-10-25 21:02:37 -0300

    Add ABMs Planes y Especialidades

---
+++ UI.Web/Especialidades.aspx.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Business.Logic;
+using Business.Entities;
+
+namespace UI.Web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack) // Verifica que no se una nueva visita (primera carga)
+            {
+                if (Session["Usuario"] != null)
+                {
+                    LoadGrid();
+                }
+                else
+                {
+                    Page.Response.Redirect("~/Login.aspx");
+                }
+            }
+        }
+        EspecialidadLogic _logic;
+        private EspecialidadLogic Logic
+        {
+            get
+            {
+                if (_logic == null)
+                {
+                    _logic = new EspecialidadLogic();
+                }
+                return _logic;
+            }
+        }
+        private void LoadGrid()
+        {
+            this.gridView.DataSource = this.Logic.GetAll();
+            this.gridView.DataBind();
+        }
+        public enum FormModes
+        {
+            Alta,
+            Baja,
+            Modificacion
+        }
+        public FormModes FormMode
+        {
+            get { return (FormModes)this.ViewState["FormMode"]; }
+            set { this.ViewState["FormMode"] = value; }
+        }
+        private Especialidad Entity
+        {
+            get;
+            set;
+        }
+        private int SelectedID
+        {
+            get
+            {
+                if (this.ViewState["SelectedID"] != null)
+                {
+                    return (int)this.ViewState["SelectedID"];
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                this.ViewState["SelectedID"] = value;
+            }
+        }
+
+        private bool IsEntitySelected
+        {
+            get
+            {
+                return (this.SelectedID != 0);
+            }
+        }
+
+        protected void gridView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.SelectedID = (int)this.gridView.SelectedValue;
+        }
+
+        private void LoadForm(int id)
+        {
+            this.Entity = this.Logic.GetOne(id);
+            this.descTextBox.Text = this.Entity.Descripcion;
+        }
+
+        protected void editarLinkButrron_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.gridPanel.Visible = false;
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Modificacion;
+                this.EnableForm(true);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void LoadEntity(Especialidad esp)
+        {
+            esp.Descripcion = this.descTextBox.Text;
+        }
+
+        private void SaveEntity(Especialidad esp)
+        {
+            if (Page.IsValid)
+            {
+                this.Logic.Save(esp);
+            }
+        }
+
+        protected void aceptarLinkButton_Click(object sender, EventArgs e)
+        {
+            switch (this.FormMode)
+            {
+                case FormModes.Baja:
+                    this.DeleteEntity(this.SelectedID);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Modificacion:
+                    this.Entity = new Especialidad();
+                    this.Entity.ID = this.SelectedID;
+                    this.Entity.State = BusinessEntity.States.Modified;
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                case FormModes.Alta:
+                    this.Entity = new Especialidad();
+                    this.LoadEntity(this.Entity);
+                    this.SaveEntity(this.Entity);
+                    this.LoadGrid();
+                    break;
+                default:
+                    break;
+            }
+            this.formPanel.Visible = false;
+            this.gridPanel.Visible = true;
+        }
+
+        private void EnableForm(bool enable)
+        {
+            this.descTextBox.Enabled = enable;
+        }
+
+        protected void eliminarLinkButton_Click(object sender, EventArgs e)
+        {
+            if (this.IsEntitySelected)
+            {
+                this.gridPanel.Visible = false;
+                this.formPanel.Visible = true;
+                this.FormMode = FormModes.Baja;
+                this.EnableForm(false);
+                this.LoadForm(this.SelectedID);
+            }
+        }
+
+        private void DeleteEntity(int id)
+        {
+            this.Logic.Delete(id);
+        }
+
+        protected void nuevoLinkButton_Click(object sender, EventArgs e)
+        {
+            this.gridPanel.Visible = false;
+            this.formPanel.Visible = true;
+            this.FormMode = FormModes.Alta;
+            this.ClearForm();
+            this.EnableForm(true);
+
+        }
+
+        private void ClearForm()
+        {
+            this.descTextBox.Text = string.Empty;
+        }
+
+        protected void cancelarLinkButton_Click(object sender, EventArgs e)
+        {
+            this.formPanel.Visible = false;
+            this.gridPanel.Visible = true;
+        }
+    }
+}
---
+++ UI.Web/Planes.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Planes.aspx.cs" Inherits="UI.Web.Planes" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="bodyContentPlaceHolder" runat="server">
+    <asp:Panel ID="gridPanel" runat="server">
+        <asp:GridView ID="gridView" runat="server" AutoGenerateColumns="False" 
+            SelectedRowStyle-BackColor="Black" SelectedRowStyle-ForeColor="White" 
+            DataKeyNames="ID" OnSelectedIndexChanged="gridView_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField HeaderText="Descripcion" DataField="Descripcion" />
+                <asp:BoundField HeaderText="Especialidad" DataField="Especialidad" /> 
+                <asp:CommandField SelectText="Seleccionar" ShowSelectButton="True" />
+            </Columns>           
+        </asp:GridView>
+        <asp:Panel ID="gridActionsPanel" runat="server">
+            <asp:LinkButton ID="editarLinkButrron" runat="server" OnClick="editarLinkButrron_Click">Editar</asp:LinkButton>
+            <asp:LinkButton ID="eliminarLinkButton" runat="server" OnClick="eliminarLinkButton_Click">Eliminar</asp:LinkButton>
+            <asp:LinkButton ID="nuevoLinkButton" runat="server" OnClick="nuevoLinkButton_Click">Nuevo</asp:LinkButton>
+        </asp:Panel>
+    </asp:Panel>
+    <asp:Panel ID="formPanel" runat="server" Visible="false">
+        <asp:Label ID="descLabel" runat="server" Text="Descripcion: "></asp:Label>
+        <asp:TextBox ID="descTextBox" ValidationGroup="ValidationGroup" runat="server"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="validatorDesc" runat="server" ForeColor="Red" ValidationGroup="ValidationGroup" ControlToValidate="descTextBox" ErrorMessage="El nombre no puede estar vacio">*</asp:RequiredFieldValidator>
+        <br />
+        <asp:Label ID="especialidadLabel" runat="server" Text="Especialidad: "></asp:Label>
+        <asp:DropDownList ID="especialidadList" runat="server" ></asp:DropDownList>
+        <asp:Panel ID="formActionsPanel" runat="server">
+            <asp:LinkButton ID="aceptarLinkButton" runat="server" OnClick="aceptarLinkButton_Click" ValidationGroup="ValidationGroup">Aceptar</asp:LinkButton>
+            <asp:LinkButton ID="cancelarLinkButton" runat="server" OnClick="cancelarLinkButton_Click">Cancelar</asp:LinkButton>
+            <asp:ValidationSummary ID="ValidationSummary" runat="server" ShowMessageBox="True" ForeColor="Red" ValidationGroup="ValidationGroup" />
+        </asp:Panel>
+    </asp:Panel>
+</asp:Content>
