12ffa7299a9e3493d7c82e133e5a23a35f2b81fd
German Birchner <birchnerg@gmail.com>
2021-11-03 19:58:34 -0300

    ReportePlanesOk

--- UI.Desktop/Planes.Designer.cs
+++ UI.Desktop/Planes.Designer.cs
@@ -42,7 +42,7 @@ namespace UI.Desktop
             this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
             this.tsbEditar = new System.Windows.Forms.ToolStripButton();
             this.tsbEliminar = new System.Windows.Forms.ToolStripButton();
-            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tsbReporte = new System.Windows.Forms.ToolStripButton();
             this.tscPlanes.ContentPanel.SuspendLayout();
             this.tscPlanes.TopToolStripPanel.SuspendLayout();
             this.tscPlanes.SuspendLayout();
@@ -163,10 +163,10 @@ namespace UI.Desktop
             this.tsbNuevo,
             this.tsbEditar,
             this.tsbEliminar,
-            this.toolStripButton1});
+            this.tsbReporte});
             this.tsPlanes.Location = new System.Drawing.Point(4, 0);
             this.tsPlanes.Name = "tsPlanes";
-            this.tsPlanes.Size = new System.Drawing.Size(139, 27);
+            this.tsPlanes.Size = new System.Drawing.Size(108, 27);
             this.tsPlanes.TabIndex = 0;
             // 
             // tsbNuevo
@@ -202,15 +202,15 @@ namespace UI.Desktop
             this.tsbEliminar.ToolTipText = "Eliminar";
             this.tsbEliminar.Click += new System.EventHandler(this.tsbEliminar_Click);
             // 
-            // toolStripButton1
+            // tsbReporte
             // 
-            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
-            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.toolStripButton1.Name = "toolStripButton1";
-            this.toolStripButton1.Size = new System.Drawing.Size(24, 24);
-            this.toolStripButton1.Text = "toolStripButton1";
-            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            this.tsbReporte.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbReporte.Image = ((System.Drawing.Image)(resources.GetObject("tsbReporte.Image")));
+            this.tsbReporte.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbReporte.Name = "tsbReporte";
+            this.tsbReporte.Size = new System.Drawing.Size(24, 24);
+            this.tsbReporte.Text = "Reporte";
+            this.tsbReporte.Click += new System.EventHandler(this.toolStripButton1_Click);
             // 
             // Planes
             // 
@@ -249,6 +249,6 @@ namespace UI.Desktop
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn desc_plan;
         private System.Windows.Forms.DataGridViewTextBoxColumn especialidad;
-        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton tsbReporte;
     }
 }
